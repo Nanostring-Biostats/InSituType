051b8914ba774228ea9e2db17821c6507ba098bf
patrickjdanaher <patrickjdanaher@gmail.com>
2021-11-02 13:18:28 -0700

    flightpath functions and tests

---
+++ tests/testthat/test_flightpath.R
@@ -0,0 +1,34 @@
+
+# simulate probs and profiles:
+celltypes <- letters[1:6]
+probs <- matrix(runif(600), 100)
+probs <- sweep(probs, 1, rowSums(probs), "/")
+profiles <- matrix(rgamma(300, 1), 50)
+colnames(probs) <- colnames(profiles) <- celltypes
+res <- list(probs = probs, profiles = profiles, clust = colnames(probs)[apply(probs,1,which.max)])
+
+
+# test flightpath_layout
+fp <- flightpath_layout(res$probs, res$profiles)
+test_that("flightpath_layout returns correct format", {
+  expect_true(all(dim(fp$clustpos) == c(6,2)))
+  expect_true(all(dim(fp$cellpos) == c(100,2)))
+  expect_true(all(!is.na(fp$clustpos)))
+  expect_true(all(!is.na(fp$cellpos)))
+})
+
+
+# test flightpath_plot from flightpath results
+p <- flightpath_plot(flightpath_result = fp)
+test_that("flightpath_plot returns a ggplot object", {
+  expect_true(any(grepl("gg", class(p))))
+})
+
+
+# test flightpath_plot from insitutype results
+p <- flightpath_plot(insitutype_result = res)
+test_that("flightpath_plot returns a ggplot object", {
+  expect_true(any(grepl("gg", class(p))))
+})
+
+
