ada44809e5dc04acfadfbd614568aa0a216c63b1
patrickjdanaher <patrickjdanaher@gmail.com>
2022-05-03 15:16:37 -0700

    change argument order

--- R/flightpath_layout.R
+++ R/flightpath_layout.R
@@ -3,8 +3,8 @@
 #' "Flightpath" (umap-like) plot of clustering results
 #'
 #' Arrays cells in 2d space based on their probability of belonging to a given cluster.
-#' @param probs Matrix of cells' probabilities of belonging to each cluster. Must provide this or logliks argument.
 #' @param logliks Matrix of cells' log-likelihoods under each cluster. Must provide this or probs argument.
+#' @param probs Matrix of cells' probabilities of belonging to each cluster. Must provide this or logliks argument.
 #' @param profiles Matrix of cell type mean expression profiles. If provided, profiles rather than probs will be used to lay out the centroids. 
 #' @param cluster_xpos Vector of cluster centroids' x positions (i.e. where you want each cell type to appear in the plot)
 #' @param cluster_ypos Vector of cluster centroids' y positions
@@ -15,7 +15,7 @@
 #' }
 #' @importFrom umap umap
 #' @export
-flightpath_layout <- function(probs = NULL, logliks = NULL, profiles = NULL, cluster_xpos = NULL, cluster_ypos = NULL) {
+flightpath_layout <- function(logliks = NULL, probs = NULL, profiles = NULL, cluster_xpos = NULL, cluster_ypos = NULL) {
 
   if (is.null(probs) & is.null(logliks)) {
     stop("Must provide either probs or logliks.")
