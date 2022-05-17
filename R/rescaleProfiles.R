912dfe9e35d400ab03e8d27b6850587e4a7cbb46
patrickjdanaher <patrickjdanaher@gmail.com>
2022-05-17 16:24:52 -0700

    correct docs

--- R/rescaleProfiles.R
+++ R/rescaleProfiles.R
@@ -9,10 +9,13 @@
 #' @param align_genes Logical, for whether to align the counts matrix and the fixed_profiles by gene ID.
 #' @param nb_size The size parameter to assume for the NB distribution.
 #' @param max_rescaling Scaling factors will be truncated above by this value and below by its inverse (at 1/value and value)
-#' @return A profiles matrix with the rows rescaled according to platform effects, 
-#'  and a vector of genes' scaling factors (what they were multiplied by when updating the reference profiles). 
+#' @return 
+#' \enumerate{
+#' \item profiles: A profiles matrix with the rows rescaled according to platform effects
+#' \item scaling_factors: A vector of genes' scaling factors (what they were multiplied by when updating the reference profiles). 
+#' }
 #' @export
-#' @importFrom MASS glm.nb
+#' @importFrom SpatialDecon spatialdecon
 rescaleProfiles <- function(counts, neg, fixed_profiles, align_genes = TRUE, nb_size = 10, max_rescaling = 5) {
   
   # align genes:
@@ -40,7 +43,7 @@ rescaleProfiles <- function(counts, neg, fixed_profiles, align_genes = TRUE, nb_
   totcountsbgsub <- pmax(totcounts - sum(neg), 0)
   
   # deconvolve the bulk profile with spatialdecon:
-  res <- spatialdecon(norm = cbind(totcounts, totcounts), 
+  res <- SpatialDecon::spatialdecon(norm = cbind(totcounts, totcounts), 
                       bg = sum(neg), 
                       X = fixed_profiles, 
                       resid_thresh = Inf)
@@ -55,3 +58,54 @@ rescaleProfiles <- function(counts, neg, fixed_profiles, align_genes = TRUE, nb_
   return(out)
 }
   
+
+
+
+#' Use anchor cells to update reference profiles
+#' 
+#' Uses anchor cells to estimate platform effects / scaling factors to be applied to
+#'  the genes/rows of the reference profile matrix. Then uses Bayesian math to
+#'  update the individual elements on X. 
+#' @param counts Counts matrix, cells * genes.
+#' @param neg Vector of mean negprobe counts per cell. Can be provided 
+#' @param anchors Vector of anchor assignments
+#' @param fixed_profiles Matrix of expression profiles of pre-defined clusters,
+#'  e.g. from previous scRNA-seq. These profiles will not be updated by the EM algorithm.
+#'  Colnames must all be included in the init_clust variable.
+#' @param align_genes Logical, for whether to align the counts matrix and the fixed_profiles by gene ID.
+#' @param nb_size The size parameter to assume for the NB distribution.
+#' @param max_rescaling Scaling factors will be truncated above by this value and below by its inverse (at 1/value and value)
+#' @return 
+#' \enumerate{
+#' \item profiles: A profiles matrix with the rows rescaled according to platform effects and individual elements updated further
+#' \item scaling_factors: A vector of genes' scaling factors (what they were multiplied by when updating the reference profiles). 
+#' }
+#' @export
+rescaleProfiles <- function(counts, neg, anchors, fixed_profiles, align_genes = TRUE, nb_size = 10, max_rescaling = 5) {
+  
+  # get total expression and negs per anchor cell type:
+  sums <- sapply(by(counts, anchors, colSums), cbind)
+  rownames(sums) <- colnames(counts)
+  temp <- by(neg, anchors, sum)
+  negsums <- as.vector(temp)
+  names(negsums) <- names(temp)
+  negsums <- negsums[colnames(sums)]
+  
+  ### build data frame for estimating row/gene scaling factors:
+  celltypes <- intersect(colnames(fixed_profiles), unique(anchors))
+  genes <- intersect(rownames(sums), rownames(fixed_profiles))
+  df <- data.frame(
+    gene = rep(genes, length(celltypes)),
+    celltype = rep(celltypes, each = lenght(genes)),
+    ref = as.vector(fixed_profiles[genes, celltypes]),
+    rnasum = as.vector(sums[genes, celltypes]),
+    negsum = rep(negsums, each = length(genes))
+  )
+  df$bgsub <- pmax(df$rnasum - df$negsum, 0)
+  # throw out rows with values too low to be useful in estimating a log-ratio:
+  
+  
+  
+  
+  
+}
