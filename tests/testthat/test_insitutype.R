c6dfa70e99805167b962ec23ff52b91abfbe2845
patrickjdanaher <patrickjdanaher@gmail.com>
2022-08-08 11:26:32 -0700

    remove rescaleProfiles (spatialdecon dependency)

--- tests/testthat/test_insitutype.R
+++ tests/testthat/test_insitutype.R
@@ -349,13 +349,16 @@ testthat::test_that("refineClusters works when merges and deletions are asked fo
 
 
 # test rescaleProfiles:
-rescaled <- rescaleProfiles(counts = mini_nsclc$counts,
-                            neg = mini_nsclc$neg, 
-                            reference_profiles = ioprofiles, 
-                            max_rescaling = 5)
-testthat::test_that("rescaleProfiles works as intended", {
-  expect_true(is.matrix(rescaled$profiles))
-  expect_true(is.vector(rescaled$scaling_factors))
-  expect_true(all(rescaled$scaling_factors >= 1/5))
-  expect_true(all(rescaled$scaling_factors <= 5))
-})
+if (FALSE) {
+  rescaled <- rescaleProfiles(counts = mini_nsclc$counts,
+                              neg = mini_nsclc$neg, 
+                              reference_profiles = ioprofiles, 
+                              max_rescaling = 5)
+  testthat::test_that("rescaleProfiles works as intended", {
+    expect_true(is.matrix(rescaled$profiles))
+    expect_true(is.vector(rescaled$scaling_factors))
+    expect_true(all(rescaled$scaling_factors >= 1/5))
+    expect_true(all(rescaled$scaling_factors <= 5))
+  })
+  
+}
