b6fb27af56084d0a24f596b65f115742579783ba
r2ds <r2ds@Siddharth-Bhatis-Macbook-Pro.local>
2022-09-08 00:56:56 +0530

    file name without spaces

--- R/flightpath_layout.R
+++ R/flightpath_layout.R
@@ -135,7 +135,7 @@ flightpath_plot <- function(flightpath_result = NULL, insitutype_result = NULL, 
           axis.text = ggplot2::element_blank())
   flightpath_plot_folder <- "/tmp/NBClust-Plots"# tempdir()
   if (!dir.exists(flightpath_plot_folder)) dir.create(flightpath_plot_folder)
-  flightpath_plot_filename <- paste(format(Sys.time(), "%Y-%m-%d_%H-%M-%S-%Z"), "flightpath_plot.png")
+  flightpath_plot_filename <- paste(format(Sys.time(), "%Y-%m-%d_%H-%M-%S-%Z"), "flightpath_plot.png", sep="-")
   flightpath_plot_file <- paste(flightpath_plot_folder,flightpath_plot_filename , sep="/")
   message("Saving flightpath_plot to: ", flightpath_plot_file)
   ggsave(filename = flightpath_plot_filename, plot = p, device = "png", path = flightpath_plot_folder)
